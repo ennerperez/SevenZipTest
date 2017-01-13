97a9c6d0c30d132f7081eae637db35e53a713959
Enner Pérez <ennerperez@gmail.com>
2017-01-13 14:28:02 -0400

    Agregar archivos de proyecto.

---
+++ SevenZipTestCSharp/Program.cs
@@ -0,0 +1,96 @@
+﻿using SevenZip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SevenZipTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            SevenZipCompressor.SetLibraryPath(Path.Combine(IntPtr.Size == 4 ? "x86" : "x64", "7z.dll"));
+            var compressor = new SevenZipCompressor();
+            var target = args[0];
+            var chunkSize = 0;
+            if (args.Count() == 3) int.TryParse(args[2], out chunkSize);
+
+#if DEBUG
+            if (!File.Exists(target))
+#endif
+                compressor.CompressFiles(target, args[1]);
+
+            var sha1 = new List<string>();
+
+            if (File.Exists(target))
+                sha1.AddRange(SplitFile(target, int.Parse(args[2])));
+            else
+                sha1.Add(GetHash(File.ReadAllBytes(target)));
+
+            File.WriteAllText(target + ".sha", string.Join(Environment.NewLine, sha1));
+
+        }
+
+        public static string GetHash(byte[] buffer)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                byte[] hash = sha1.ComputeHash(buffer);
+                StringBuilder formatted = new StringBuilder(2 * hash.Length);
+                foreach (byte b in hash)
+                {
+                    formatted.AppendFormat("{0:X2}", b);
+                }
+
+                return formatted.ToString();
+            }
+        }
+
+        public static IEnumerable<string> SplitFile(string inputFile, int chunkSize)
+        {
+
+            var result = new List<string>();
+
+            const int BUFFER_SIZE = 20 * 1024;
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            using (Stream input = File.OpenRead(inputFile))
+            {
+                var parts = (int)Math.Ceiling((decimal)input.Length / chunkSize);
+                int index = 0;
+                while (input.Position < input.Length)
+                {
+                    var filename = inputFile + String.Format(".{0:D" + (parts.ToString().Length < 3 ? 3 : parts.ToString().Length) + "}", (index + 1));
+                    using (Stream output = File.Create(filename))
+                    {
+                        int remaining = chunkSize, bytesRead;
+                        while (remaining > 0 && (bytesRead = input.Read(buffer, 0,
+                                Math.Min(remaining, BUFFER_SIZE))) > 0)
+                        {
+                            output.Write(buffer, 0, bytesRead);
+
+                            remaining -= bytesRead;
+                        }
+                    }
+                    result.Add(GetHash(System.IO.File.ReadAllBytes(filename)));
+                    index++;
+                    Thread.Sleep(500); // experimental; perhaps try it
+                }
+            }
+
+#if !DEBUG
+            System.IO.File.Delete(inputFile);
+#endif
+
+            return result.AsEnumerable();
+
+        }
+
+    }
+}
---
+++ SevenZipTestVB/Program.vb
@@ -0,0 +1,87 @@
+﻿Imports SevenZip
+Imports System.IO
+Imports System.Threading
+Imports System.Security.Cryptography
+Imports System.Text
+
+Module Program
+
+    Sub Main()
+
+        Dim args = Environment.GetCommandLineArgs()
+
+        SevenZipCompressor.SetLibraryPath(Path.Combine(IIf(IntPtr.Size = 4, "x86", "x64"), "7z.dll"))
+        Dim compressor = New SevenZipCompressor
+        Dim target = args(1)
+        Dim chunkSize As Integer = 0
+        If args.Count() = 4 Then Integer.TryParse(args(3), chunkSize)
+
+#If DEBUG Then
+        If Not File.Exists(target) Then
+#End If
+            compressor.CompressFiles(target, args(2))
+#If DEBUG Then
+        End If
+#End If
+
+        Dim sha1 As New List(Of String)
+
+        If chunkSize > 0 AndAlso File.Exists(target) Then
+            sha1.AddRange(SplitFile(target, chunkSize))
+        Else
+            sha1.Add(GetHash(File.ReadAllBytes(target)))
+        End If
+
+        File.WriteAllText(target & ".sha", String.Join(Environment.NewLine, sha1))
+
+
+    End Sub
+
+    Public Function SplitFile(inputFile As String, chunkSize As Integer) As IEnumerable(Of String)
+        Dim result As New List(Of String)
+
+
+        Dim BUFFER_SIZE = 20 * 1024
+        Dim buffer As Byte() = New Byte(BUFFER_SIZE) {}
+
+        Using input = File.OpenRead(inputFile)
+            Dim parts = CInt(Math.Ceiling(input.Length / chunkSize))
+            Dim index = 0
+            While input.Position < input.Length
+                Dim filename = inputFile & String.Format(".{0:D" + IIf(parts.ToString().Length < 3, 3, parts.ToString().Length).ToString + "}", (index + 1))
+                Using output = File.Create(filename)
+                    Dim remaining = chunkSize
+                    Dim bytesRead As Integer = 0
+                    While remaining > 0 AndAlso bytesRead >= 0
+                        bytesRead = input.Read(buffer, 0, Math.Min(remaining, BUFFER_SIZE))
+                        If bytesRead = 0 Then Exit While
+                        output.Write(buffer, 0, bytesRead)
+                        remaining -= bytesRead
+                    End While
+                End Using
+                result.Add(GetHash(File.ReadAllBytes(filename)))
+                index += 1
+                Thread.Sleep(500) 'experimental perhaps Try it
+            End While
+        End Using
+
+#If Not DEBUG Then
+        File.Delete(inputFile)
+#End If
+
+        Return result.AsEnumerable()
+    End Function
+
+    Public Function GetHash(buffer As Byte()) As String
+        Using sha1 = New SHA1Managed()
+            Dim hash = sha1.ComputeHash(buffer)
+            Dim formatted = New StringBuilder(2 * hash.Length)
+            For Each b As Byte In hash
+                formatted.AppendFormat("{0:X2}", b)
+            Next
+            Return formatted.ToString()
+        End Using
+    End Function
+
+
+End Module
